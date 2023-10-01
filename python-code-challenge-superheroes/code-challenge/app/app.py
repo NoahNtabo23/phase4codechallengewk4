2a2d3a23c1d72cf07104f3c7e6360177961dc385
Noah <noah.ntabo@student.moringaschool.com>
2023-10-01 23:03:47 +0300

    Create routes.py

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask
 from flask_migrate import Migrate
+from flask_restful import Api
 
-from models import db, Hero
+
+from models import db
+from routes import HeroesResource, HeroResource, PowersResource, PowerResource, UpdatePowerResource, HeroPowersResource
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
+api=Api(app)
 
 db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    return 'THIS IS FLASK CODE-CHALLENGE RESTAURANTS.'
+
+api.add_resource(HeroesResource, '/heroes')
+api.add_resource(HeroResource, '/heroes/<int:id>')
+api.add_resource(PowersResource, '/powers')
+api.add_resource(PowerResource, '/powers/<int:id>')
+api.add_resource(UpdatePowerResource, '/powers/<int:id>')
+api.add_resource(HeroPowersResource, '/hero_powers')
 
 
 if __name__ == '__main__':
---
+++ python-code-challenge-superheroes/code-challenge/app/routes.py
@@ -0,0 +1,63 @@
+from flask import jsonify, request, abort
+from models import db
+from models import Hero, Powers, Hero_Powers
+from flask_restful import Resource
+
+class HeroesResource(Resource):
+    def get(self):
+        heroes = Hero.query.all()
+        return jsonify([{'id': hero.id, 'name': hero.name, 'super_name': hero.super_name} for hero in heroes])
+
+class HeroResource(Resource):
+    def get(self, id):
+        hero = Hero.query.get(id)
+        if not hero:
+            abort(404, error='Hero not found')
+        powers = [{'id': power.id, 'name': power.name, 'description': power.description} for power in hero.powers]
+        return jsonify({'id': hero.id, 'name': hero.name, 'super_name': hero.super_name, 'powers': powers})
+
+class PowersResource(Resource):
+    def get(self):
+        powers = Powers.query.all()
+        return jsonify([{'id': power.id, 'name': power.name, 'description': power.description} for power in powers])
+
+class PowerResource(Resource):
+    def get(self, id):
+        power = Powers.query.get(id)
+        if not power:
+            abort(404, error='Power not found')
+        return jsonify({'id': power.id, 'name': power.name, 'description': power.description})
+
+class UpdatePowerResource(Resource):
+    def patch(self, id):
+        power = Powers.query.get(id)
+        if not power:
+            abort(404, error='Power not found')
+        data = request.get_json()
+        if 'description' not in data or len(data['description']) < 20:
+            return {'errors': ['validation errors']}, 400
+        power.description = data['description']
+        db.session.commit()
+        return jsonify({'id': power.id, 'name': power.name, 'description': power.description})
+
+class HeroPowersResource(Resource):
+    def post(self):
+        data = request.get_json()
+        strength = data.get('strength')
+        power_id = data.get('power_id')
+        hero_id = data.get('hero_id')
+        
+        if not all([strength, power_id, hero_id]):
+            return {'errors': ['validation errors']}, 400
+        
+        power = Powers.query.get(power_id)
+        hero = Hero.query.get(hero_id)
+        
+        if not power or not hero:
+            abort(404, error='Power or Hero not found')
+        
+        hero_power = Hero_Powers(strength=strength, hero=hero, power=power)
+        db.session.add(hero_power)
+        db.session.commit()
+        
+        return jsonify({'id': hero.id, 'name': hero.name, 'super_name': hero.super_name, 'powers': [{'id': power.id, 'name': power.name, 'description': power.description}]}), 201
